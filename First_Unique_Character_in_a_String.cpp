a109dfd4bcdd7b309d9d93ee50fe92914ba3f019
Gurmeet Singh <40573801+Gurmeetsingh8@users.noreply.github.com>
2020-05-05 15:11:29 +0530

    Add files via upload

---
+++ First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int>m;
+        for(int i=0;i<s.size();i++)
+        {
+            if(m.find(s[i])==m.end())
+                m[s[i]]=1;
+            else
+                m[s[i]]++;
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            if(m[s[i]]==1)
+                return i;
+        }
+        return -1;
+
+    }
+};
